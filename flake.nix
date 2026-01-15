6f70d95167ce328e653e205c0bff4402df5544e9
peigongdsd <peigong2013@outlook.com>
2026-01-15 20:22:29 +0000

    Add formatter and some minor tweaks to nixosModule
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -6,24 +6,30 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     let
-      mkForSystem = system:
+      mkForSystem =
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
           lib = pkgs.lib;
 
           yggoverlay-cni-plugin = pkgs.buildGoModule {
             pname = "yggoverlay-cni-plugin";
-            version =
-              if self ? rev
-              then lib.substring 0 8 self.rev
-              else "dirty";
+            version = if self ? rev then lib.substring 0 8 self.rev else "dirty";
 
             src = self;
 
             vendorHash = "sha256-h44omAFdmWFE19ocKWTCIhWKd/J4pV5uPxHg6tqPOAo=";
-            ldflags = [ "-s" "-w" ];
+            ldflags = [
+              "-s"
+              "-w"
+            ];
             subPackages = [ "." ];
 
             postInstall = ''
@@ -53,10 +59,15 @@
           #   - yggoverlay
           cni-plugins-with-yggoverlay = pkgs.symlinkJoin {
             name = "cni-plugins-with-yggoverlay";
-            paths = [ pkgs.cni-plugins yggoverlay-cni-plugin ];
+            paths = [
+              pkgs.cni-plugins
+              yggoverlay-cni-plugin
+            ];
           };
         in
         {
+          formatter = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+
           packages = {
             default = yggoverlay-cni-plugin;
             inherit yggoverlay-cni-plugin cni-plugins-with-yggoverlay;
@@ -77,29 +88,35 @@
           };
         };
     in
-      flake-utils.lib.eachDefaultSystem mkForSystem
-      //
-      {
-        nixosModules.default = { config, pkgs, lib, ... }:
-          let
-            cfg = config.virtualisation.containerd.yggoverlayCNI;
-            bundle = self.packages.${pkgs.system}.cni-plugins-with-yggoverlay;
-          in
-          {
-            options.virtualisation.containerd.yggoverlayCNI.enable =
-              lib.mkEnableOption "Install yggoverlay CNI plugin and point containerd's CNI bin_dir at a merged plugin bundle";
-
-            config = lib.mkIf cfg.enable {
-              virtualisation.containerd.enable = true;
-
-              # containerd CRI plugin CNI paths: bin_dir, conf_dir
-              # (bin_dir default is /opt/cni/bin; conf_dir default is /etc/cni/net.d). :contentReference[oaicite:1]{index=1}
-              virtualisation.containerd.settings.plugins."io.containerd.grpc.v1.cri".cni = {
-                bin_dir = "${bundle}/bin";
-                conf_dir = "/etc/cni/net.d";
-              };
-            };
+    flake-utils.lib.eachDefaultSystem mkForSystem
+    // {
+      nixosModules.default =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        let
+          cfg = config.virtualisation.containerd.yggoverlayCNI;
+          bundle = self.packages.${pkgs.system}.cni-plugins-with-yggoverlay;
+        in
+        {
+          options.virtualisation.containerd.yggoverlayCNI = {
+            enable = lib.mkEnableOption "Install yggoverlay CNI plugin and point containerd's CNI bin_dir at a merged plugin bundle";
+	    
           };
-      };
-}
 
+          config = lib.mkIf cfg.enable {
+            # containerd CRI plugin CNI paths: bin_dir, conf_dir
+            # (bin_dir default is /opt/cni/bin; conf_dir default is /etc/cni/net.d).
+            virtualisation.containerd.settings.plugins."io.containerd.grpc.v1.cri".cni = {
+              bin_dir = "${bundle}/bin";
+              conf_dir = "/etc/cni/net.d";
+            };
+
+            environment.systemPackages = [ pkgs.nerdctl ];
+          };
+        };
+    };
+}
